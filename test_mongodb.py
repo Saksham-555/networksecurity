98f59ee2dabb02aaf0da17e2857a7c33149d2a60
SAKSHAM AGARWAL <agarwalsaksham11@gmail.com>
2025-10-07 19:40:51 +0530

    data ingestion component commited

--- test_mongodb.py
+++ test_mongodb.py
@@ -1,7 +1,7 @@
 
 from pymongo.mongo_client import MongoClient
 
-uri = "mongodb+srv://sakshamworkflow_db_user:Admin123@cluster0.uk0ldti.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://sakshamworkflow_db_user:<your password>cluster0.uk0ldti.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri)
